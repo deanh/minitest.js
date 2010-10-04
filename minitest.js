7674989b53f30d8d4f6c52b0db4fd08e266a42c0
this guy <dean@ero.com>
2010-10-03 18:27:15 -0700

    added README

--- minitest.js
+++ minitest.js
@@ -2,7 +2,9 @@
 //
 // A small unit testing framework based on Ryan Davis' MiniTest::Unit
 //
-// Copyright (c) 2010 Harry Dean Hudson, <dean@ero.com>
+// Copyright (c) 2010 Harry Dean Hudson Jr., <dean@ero.com>
+//
+// Much thanks to Ryan and Seattle.rb
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
